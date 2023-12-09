3798cdb3066a77f31344f1f9a3d0f14d726360a3
prastowoardi <prastowoardi616@gmail.com>
2023-12-09 13:57:44 +0700

    integration registration page

--- js/auth.js
+++ js/auth.js
@@ -38,6 +38,7 @@ function login() {
         console.log('Body:', body);
         if (body.success) {
             console.log('Selamat anda berhasil login');
+            window.location.href = "#";
         } else {
             displayErrorMessage('Terjadi kesalahan saat melakukan login');
         }
@@ -48,10 +49,73 @@ function login() {
     });
 }
 
+function register() {
+    clearErrorMessages();
+
+    const name = document.getElementById('name').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    const fields = [
+        { field: 'name', value: name, message: 'Nama harus diisi' },
+        { field: 'username', value: username, message: 'Username harus diisi' },
+        { field: 'password', value: password, message: 'Password harus diisi' },
+        { field: 'confirm-password', value: confirmPassword, message: 'Konfirmasi Password harus diisi' }
+    ];
+
+    fields.forEach(field => {
+        if (!field.value) {
+            displayFieldMessage(field.field, field.message);
+        }
+    });
+
+    if (password !== confirmPassword) {
+        displayFieldMessage('confirm-password', 'Konfirmasi Password tidak cocok');
+        return;
+    }
+
+    const data = {
+        name: name,
+        username: username,
+        password: password,
+        roleId: 1,
+    };
+
+    fetch('https://api.mudoapi.tech/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(body => {
+            console.log('Body:', body);
+            if (body.success) {
+                console.log('Selamat anda berhasil Mendaftar');
+                window.location.href = "#";
+            } else {
+                console.log(body.message);
+            }
+        })
+        .catch(error => {
+            console.error('Fetch Error:', error);
+            displayErrorMessage(body.message);
+        });
+}
+
 function clearErrorMessages() {
-    document.getElementById("error-message").innerHTML = "";
-    document.getElementById("username-message").innerHTML = "";
-    document.getElementById("password-message").innerHTML = "";
+    const errorFields = ["name", "username", "password", "confirm-password"];
+
+    errorFields.forEach(fieldName => {
+        const element = document.getElementById(fieldName + "-message");
+        if (element) {
+            element.innerHTML = "";
+        }
+    });
 }
 
 function displayErrorMessage(message) {
@@ -59,5 +123,8 @@ function displayErrorMessage(message) {
 }
 
 function displayFieldMessage(fieldName, message) {
-    document.getElementById(fieldName + "-message").innerHTML = message;
+    const element = document.getElementById(fieldName + "-message");
+    if (element) {
+        element.innerHTML = message;
+    }
 }

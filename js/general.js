203398fc285c3665b8694fed108b2818f96b6e4c
prastowoardi <prastowoardi616@gmail.com>
2023-12-09 12:44:35 +0700

    update register page

--- js/general.js
+++ js/general.js
@@ -1,15 +1,20 @@
-function showHidePassword() {
-    var passwordInput = document.getElementById("password");
-    var openEye = document.getElementById("open-eye");
-    var closedEye = document.getElementById("closed-eye");
+function showHidePassword(inputId, eyeContainerId, openEyeId, closedEyeId) {
+    var passwordInput = document.getElementById(inputId);
+    var eyeContainer = document.getElementById(eyeContainerId);
+    var openEye = document.getElementById(openEyeId);
+    var closedEye = document.getElementById(closedEyeId);
 
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        openEye.style.display = "none";
-        closedEye.style.display = "block";
+    if (passwordInput && eyeContainer && openEye && closedEye) {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            openEye.style.display = "none";
+            closedEye.style.display = "block";
+        } else {
+            passwordInput.type = "password";
+            openEye.style.display = "block";
+            closedEye.style.display = "none";
+        }
     } else {
-        passwordInput.type = "password";
-        openEye.style.display = "block";
-        closedEye.style.display = "none";
+        console.error("One or more elements not found.");
     }
 }
